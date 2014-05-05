adc7b556a2a032a680e04622d46c86c82c21ddfd
Maschenko_Olenka <maschenko.elena@yandex.ru>
2014-05-05 11:25:34 +0300

    commm

---
+++ Java/src/java/Java.java
@@ -0,0 +1,13 @@
+package java;
+
+public class Java {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 50;
+        int c = a + b;
+        System.out.println("Result" + c);
+    }
+
+}
